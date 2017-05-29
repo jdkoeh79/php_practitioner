02542a2c3b9dff9a12c7373754519d416b653071
Justin Koehler <jdkoeh79@gmail.com>
2017-05-28 23:59:50 -0500

    Laracasts - The PHP Practitioner: Classes 101 (end of lesson)

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php 
+
+// makes dd() function available (for debugging/checking variable states)
+require 'functions.php';
+
+class Task {
+
+	protected $description;
+	protected $completed = false;
+
+	public function __construct($description)
+	{
+		$this->description = $description;
+	}
+
+	public function description()
+	{
+		return $this->description;
+	}
+
+	public function complete()
+	{
+		$this->completed = true;
+	}
+
+	public function isComplete()
+	{
+		return $this->completed;
+	}
+}
+
+
+$tasks = [
+	new Task('Go to the store'),
+	new Task('Finish my screencast'),
+	new Task('Clean my room')
+];
+
+
+$tasks[0]->complete();
+
+
+require 'index.view.php';
