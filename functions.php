f39fea02188c9efd2542080f725f13314b5db011
Justin Koehler <jdkoeh79@gmail.com>
2017-05-29 21:05:41 -0500

    Laracasts - The PHP Practitioner: PDO Refactoring and Collaborators (10:55) Took the hard coded database query and moved it into a QueryBuilder class

--- functions.php
+++ functions.php
@@ -1,11 +1,5 @@
 <?php 
 
-function fetchAllTasks($pdo) {
-	$statement = $pdo->prepare('SELECT * FROM todos');
-	$statement->execute();
-	return $statement->fetchAll(PDO::FETCH_CLASS, 'Task');
-}
-
 function dd($data) {
 	echo '<pre>';
 	die(var_dump($data));
