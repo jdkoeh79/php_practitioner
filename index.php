47475211c4914d31835c86288f7a801b1c952d7f
Justin Koehler <jdkoeh79@gmail.com>
2017-05-29 21:33:42 -0500

    Laracasts - The PHP Practitioner: Refactoring and Collaborators (13:33) Created a bootstrap.php file to clean up some of the includes in index.php.  It returns a QueryBuilder object which is assigned to a variable upon being included in index.php

--- index.php
+++ index.php
@@ -1,13 +1,10 @@
 <?php 
 
+$query = require 'bootstrap.php';
 require 'functions.php';
-require 'database/Connection.php';
-require 'database/QueryBuilder.php';
 require 'Task.php';
 
-$pdo = Connection::make();
 
-$query = new QueryBuilder($pdo);
 
 $tasks = $query->selectAll('todos');
 
